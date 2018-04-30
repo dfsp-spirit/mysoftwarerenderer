94312d6f8b1b889683506025f90edb0b9f2683d9
Tim Schaefer <ts+code@rcmd.org>
2018-04-30 12:10:34 +0200

    Removing red point from output image, change color for 3rd line.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 void line(int, int, int, int, TGAImage &, TGAColor);
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor red   = TGAColor(255, 0, 0, 255);
+const TGAColor green   = TGAColor(0, 255, 0, 255);
+const TGAColor blue   = TGAColor(0, 0, 255, 255);
 
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
-    image.set(52, 41, red);
 
     line(13, 20, 80, 40, image, white);
     line(20, 13, 40, 80, image, red);
-    line(80, 40, 13, 20, image, red);
+    line(80, 40, 13, 20, image, green);
 
 
     image.flip_vertically();
