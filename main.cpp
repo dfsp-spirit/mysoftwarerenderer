163f919fd37209e63c0b9b3bb8a2da2d9b9a554c
Tim Schaefer <ts+code@rcmd.org>
2018-04-30 16:20:08 +0200

    Use model parser and vector functions to parse and render model as a wireframe.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include <vector>
+#include <cmath>
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
 void line(int, int, int, int, TGAImage &, TGAColor);
 
@@ -7,17 +11,42 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
+
 int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
+
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+
+    TGAImage image(width, height, TGAImage::RGB);
 
     line(25, 25, 95, 95, image, white);
     line(20, 13, 40, 80, image, red);
     line(40, 80, 20, 13, image, green); // Note that this green line will overwrite the last red line
     line(10, 10, 30, 80, image, blue);
 
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j=0; j<3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j+1)%3]);
+            int x0 = (v0.x+1.)*width/2.;
+            int y0 = (v0.y+1.)*height/2.;
+            int x1 = (v1.x+1.)*width/2.;
+            int y1 = (v1.y+1.)*height/2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
+
 
     image.flip_vertically();  // Origin should be in the left bottom corner.
     image.write_tga_file("output.tga");
+    delete model;
     return 0;
 }
 
@@ -39,7 +68,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     // Set the color for each pixel.
     int deltax = x1 - x0;
     int deltay = y1 - y0;
-    int derror2 = std::abs(deltay) * 2; // We could use the error per pixel, but want to avoid floats.
+    int derror2 = std::abs(deltay) * 2; // We could use the error per pixel, but want to avoid floats. So we simply skip diving this by the number of pixels.
                                         // And to avoid having to compare this value to deltax/2 (another float) later, we multiply it by 2
                                         // and can thus compare it to deltax then.
     int current_error2 = 0;
